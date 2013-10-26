8d77d4b2b96bbcb81d66d3502127693869209bd3
Anton Baklanov <antonbaklanov@gmail.com>
2013-10-26 22:03:00 +0300

    start with basic helloworld wsgi app

--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
-import unittest
+import urllib
+from wsgitestcase import WsgiTestCase
 
 
-class TestNothing(unittest.TestCase):
+class TestNothing(WsgiTestCase):
 
-    def test_nothing(self):
-        print("nothing works fine")
+    def test_simpleserver(self):
+        req = urllib.request.urlopen(
+            "http://%s:%s/" % (self.host, self.port))
+        self.assertEqual(req.read(), b"hello world")
--- wsgitestcase.py
+++ wsgitestcase.py
@@ -1,10 +1,53 @@
 import threading
 import unittest
+import time
+import errno
+
+from wsgiref.simple_server import make_server
+
+from werkzeug.wrappers import Request, Response
+
+
+@Request.application
+def helloworld_app(request):
+    return Response("hello world")
 
 
 class WsgiThread(threading.Thread):
-    pass
+
+    def __init__(self, app, **kwargs):
+        self.app = app
+        self.port = 8000
+        self.host = "127.0.0.1"
+        self.up_and_ready = threading.Event()
+        self.error = None
+        super(WsgiThread, self).__init__(**kwargs)
+
+    def run(self):
+        self.server = make_server(self.host, self.port, self.app)
+        self.up_and_ready.set()
+        self.server.serve_forever()
+
+    def join(self):
+        self.server.shutdown()
+        super(WsgiThread, self).join()
 
 
 class WsgiTestCase(unittest.TestCase):
-    pass
+
+    app = helloworld_app
+
+    @classmethod
+    def setUpClass(cls):
+        cls.server_thread = WsgiThread(cls.app)
+        cls.server_thread.start()
+        cls.server_thread.up_and_ready.wait()
+        cls.host = cls.server_thread.host
+        cls.port = cls.server_thread.port
+        if cls.server_thread.error:
+            raise cls.server_thread.error
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server_thread.join()
+
