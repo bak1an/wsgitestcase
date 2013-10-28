652d24d08217f62446c0c64d4903d6952cb6b5ac
Anton Baklanov <antonbaklanov@gmail.com>
2013-10-28 19:45:49 +0200

    looks like overriding app in subclasses is working fine
    but you can't have two test cases in one suite

--- tests.py
+++ tests.py
@@ -1,15 +1,31 @@
 import requests
 from wsgitestcase import WsgiTestCase
 from wsgitestcase import get_cool_unittest
+from werkzeug.wrappers import Request, Response
+
 
 unittest = get_cool_unittest()
 
 
-class TestSimpleServer(WsgiTestCase):
+@Request.application
+def goodbye_app(request):
+    return Response("goodbye world")
+
+
+#class TestSimpleServer(WsgiTestCase):
+#
+#    def test_simpleserver(self):
+#        r = requests.get("http://%s:%s/" % (self.host, self.port))
+#        self.assertEqual(r.text, "hello world")
+
+
+class TestOtherApp(WsgiTestCase):
+
+    app = staticmethod(goodbye_app)
 
     def test_simpleserver(self):
         r = requests.get("http://%s:%s/" % (self.host, self.port))
-        self.assertEqual(r.text, "hello world")
+        self.assertEqual(r.text, "goodbye world")
 
 
 if __name__ == '__main__':
