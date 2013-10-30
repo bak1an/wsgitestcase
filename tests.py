25dd48bd0bdaab2626a8602d9acd67d90a5343f4
Anton Baklanov <antonbaklanov@gmail.com>
2013-10-30 20:08:37 +0200

    add more tests for ports selecting

--- tests.py
+++ tests.py
@@ -43,7 +43,11 @@ class TestPortSelect(unittest.TestCase):
             t.join()
 
     def listen_on(self, port):
-        pass
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(("127.0.0.1", port))
+        s.listen(5)
+        self.open_sockets.append(s)
 
     def run_server_thread(self):
         t = WsgiThread(goodbye_app)
@@ -52,6 +56,8 @@ class TestPortSelect(unittest.TestCase):
         if t.error:
             raise t.error
         self.threads.append(t)
+        r = requests.get("http://%s:%s/" % (t.host, t.port))
+        self.assertEqual(r.text, "goodbye world")
         return t
 
     def test_one_server(self):
@@ -64,6 +70,17 @@ class TestPortSelect(unittest.TestCase):
         self.assertEqual(t1.port, 8000)
         self.assertEqual(t2.port, 8001)
 
+    def test_busy_ports(self):
+        for port in range(8000, 8005):
+            self.listen_on(port)
+        t = self.run_server_thread()
+        self.assertEqual(t.port, 8005)
+
+    def test_all_ports_busy(self):
+        for port in range(8000, 8010):
+            self.listen_on(port)
+        with self.assertRaises(socket.error):
+            self.run_server_thread()
 
 if __name__ == '__main__':
     unittest.main()
